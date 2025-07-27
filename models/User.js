5f4db4cfddf1d1eb6f0cb16c0afb073dbe5f3cba
chiragjethva03 <chiragjethva23@gmail.com>
2025-07-28 00:19:42 +0530

    database

---
+++ models/User.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      default: null, // Will stay null if Google sign-in
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    phoneNumber: {
+      type: String,
+      default: "",
+    },
+    accountStatus: {
+      type: String,
+      enum: ["undefined", "active", "deleted"],
+      default: "undefined",
+    },
+    authProvider: {
+      type: String,
+      enum: ["manual", "google"],
+      default: "manual",
+    },
+    ticketIDs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ticket",
+      },
+    ],
+    postIDs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+    googleId: {
+      type: String, // store google's sub ID
+      default: null,
+    },
+  },
+  {
+    timestamps: true, // automatically adds createdAt and updatedAt
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
