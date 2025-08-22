6f9b751380d9f56da50216930164e39883767713
chiragjethva03 <chiragjethva23@gmail.com>
2025-08-22 15:47:52 +0530

    token error

--- app.js
+++ app.js
@@ -32,9 +32,6 @@ app.get("/", (req, res) => {
   return res.send("requested accepted.!");
 });
 
-app.get("*", (req, res, next)=> {
-  res.send("request danied.!")
-})
 
 app.listen(3000, () => {
   console.log("server start at port 3000 number");
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,27 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/User.js");
+  const jwt = require("jsonwebtoken");
+  const User = require("../models/User.js");
 
-const protect = async (req, res, next) => {
-  let token;
+  const protect = async (req, res, next) => {
+    let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+      try {
+        token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.findById(decoded.id).select("-password");
-      if (!req.user) {
-        return res.status(404).json({ message: "User not found" });
+        req.user = await User.findById(decoded.id).select("-password");
+        if (!req.user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+
+        next();
+      } catch (err) {
+        console.error(err);
+        return res.status(401).json({ message: "Not authorized, token failed" });
       }
-
-      next();
-    } catch (err) {
-      console.error(err);
-      return res.status(401).json({ message: "Not authorized, token failed" });
+    } else {
+      return res.status(401).json({ message: "No token provided" });
     }
-  } else {
-    return res.status(401).json({ message: "No token provided" });
-  }
-};
+  };
 
-module.exports = protect;
+  module.exports = protect;
